1415e969f15b9ee588839c15a388e792dddfc6f2
thang pham <kuthang32@gmail.com>
2022-04-24 21:18:33 +0700

    add apollo client + call api

--- App.tsx
+++ App.tsx
@@ -1,9 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { AppRegistry } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
+import { ApolloClient, InMemoryCache, ApolloProvider, from, HttpLink } from '@apollo/client';
+import { onError } from "@apollo/client/link/error";
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors)
+    graphQLErrors.forEach(({ message, locations, path }) =>
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+      )
+    );
+  if (networkError) console.log(`[Network error]: ${networkError}`);
+});
+// Log any GraphQL errors or network error that occurred
+
+const httpLink = new HttpLink({
+  uri: "http://6e96-2405-4802-6374-db30-f87c-dff7-bbed-c431.ngrok.io/graphql/"
+});
+
+// Initialize Apollo Client
+const client = new ApolloClient({
+  link: from([errorLink, httpLink]),
+  cache: new InMemoryCache(),
+});
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
@@ -11,12 +36,16 @@ export default function App() {
 
   if (!isLoadingComplete) {
     return null;
-  } else {
-    return (
+  }
+
+  return (
+    <ApolloProvider client={client}>
       <SafeAreaProvider>
         <Navigation colorScheme={colorScheme} />
         <StatusBar />
       </SafeAreaProvider>
-    );
-  }
+    </ApolloProvider>
+  );
 }
+
+AppRegistry.registerComponent('Orderich', () => App);
--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,15 +1,57 @@
-import { StyleSheet } from 'react-native';
+import { gql, useMutation } from '@apollo/client';
+import React from 'react';
+import { StyleSheet, TextInput, TouchableOpacity } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 
+const tokenCreate = gql`
+  mutation TokenCreate($email: String!, $password: String!) {
+    tokenCreate(email: $email, password: $password) {
+      token
+      user {
+        id
+      }
+      errors {
+        message
+        field
+      }
+    }
+  }
+`;
+
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+  const [formSubmit, setFormSubmit] = React.useState({ email: '', password: '' });
+  const [mutaFunc, { data, error, loading }] = useMutation(tokenCreate);
+  const onChangeText = (text: string, name: string) => {
+    setFormSubmit({ ...formSubmit, [name]: text });
+  };
+
+  const handleSubmit = () => {
+    mutaFunc({ variables: { ...formSubmit } });
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
+      <Text style={styles.title}>Login</Text>
+
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <TextInput
+        style={{ backgroundColor: '#fff', width: '100%', marginBottom: 10 }}
+        onChangeText={(text) => onChangeText(text, 'email')}
+        value={formSubmit.email}
+      />
+      <TextInput
+        style={{ backgroundColor: '#fff', width: '100%' }}
+        onChangeText={(text) => onChangeText(text, 'password')}
+        value={formSubmit.password}
+      />
+
+      <TouchableOpacity onPress={handleSubmit}>
+        <Text>Submit</Text>
+      </TouchableOpacity>
     </View>
   );
 }
